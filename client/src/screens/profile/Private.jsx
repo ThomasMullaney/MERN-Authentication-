c49c44449c1f750d2127df8d0129f51bdc188daf
Thomas Mullaney <tloring.mullaney@gmail.com>
2021-02-25 07:02:18 -0800

    added toggle for map on profile page

--- client/src/screens/profile/Private.jsx
+++ client/src/screens/profile/Private.jsx
@@ -1,156 +1,158 @@
-import React, { useState, useEffect } from 'react';
-import authSvg from '../../assets/update.svg';
-import { ToastContainer, toast } from 'react-toastify';
-import axios from 'axios';
-import { updateUser, isAuth, getCookie, signout } from '../../helpers/auth';
+import React, { useState, useEffect } from "react";
+import authSvg from "../../assets/update.svg";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import { updateUser, isAuth, getCookie, signout } from "../../helpers/auth";
 import "./profile.style.css";
-import Map from "./map/Map"
+import Map from "./map/Map";
 
 const Private = ({ history }) => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password1: '',
-    textChange: 'Update',
-    role: ''
+    name: "",
+    email: "",
+    password1: "",
+    textChange: "Update",
+    role: "",
+    position: "",
+    
   });
+
+  const [showMap, setShow] = useState(false)
+
+  const toggleMapOn = () => setShow(true)
+  const toggleMapOff = () => setShow(false)
 
   useEffect(() => {
     loadProfile();
   }, []);
 
+  // load Profile
   const loadProfile = () => {
-    const token = getCookie('token');
+    const token = getCookie("token");
     axios
       .get(`${process.env.REACT_APP_API_URL}/user/${isAuth()._id}`, {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .then(res => {
+      .then((res) => {
         const { role, name, email } = res.data;
         setFormData({ ...formData, role, name, email });
       })
-      .catch(err => {
+      .catch((err) => {
         toast.error(`Error To Your Information ${err.response.statusText}`);
         if (err.response.status === 401) {
           signout(() => {
-            history.push('/login');
+            history.push("/login");
           });
         }
       });
   };
+
   const { name, email, password1, textChange, role } = formData;
-  const handleChange = text => e => {
+  const handleChange = (text) => (e) => {
     setFormData({ ...formData, [text]: e.target.value });
   };
-  const handleSubmit = e => {
-    const token = getCookie('token');
+
+  // handle submit
+  const handleSubmit = (e) => {
+    const token = getCookie("token");
     console.log(token);
     e.preventDefault();
-    setFormData({ ...formData, textChange: 'Submitting' });
+    setFormData({ ...formData, textChange: "Submitting" });
     axios
       .put(
         `${process.env.REACT_APP_API_URL}/user/update`,
         {
           name,
           email,
-          password: password1
+          password: password1,
         },
         {
           headers: {
-            Authorization: `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         }
       )
-      .then(res => {
+      .then((res) => {
         updateUser(res, () => {
-          toast.success('Profile Updated Successfully');
-          setFormData({ ...formData, textChange: 'Update' });
+          toast.success("Profile Updated Successfully");
+          setFormData({ ...formData, textChange: "Update" });
         });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.response);
       });
   };
 
   return (
-    <div className='profileParentDiv flex'>
+    <div className="profileParentDiv flex">
       <ToastContainer />
-      <div className='profileViewDiv shadow  flex flex-1'>
-        <div className='profileInfoDiv'>
-          <div className='profileInfo flex'>
-            <h1 className='profileTitle'>
-              Profile Update
-            </h1>
+      <div className="profileViewDiv shadow  flex flex-1">
+        <div className="profileInfoDiv">
+          <div className="profileInfo flex">
+            <h1 className="profileTitle">Profile Update</h1>
 
-            <form
-              className='profileForm flex-1'
-              onSubmit={handleSubmit}
-            >
-              <div className='profileInputDiv'>
+            <form className="profileForm flex-1" onSubmit={handleSubmit}>
+              <div className="profileInputDiv">
                 <input
                   disabled
-                  className='profileInputs'
-                  type='text'
-                  placeholder='Role'
+                  className="profileInputs"
+                  type="text"
+                  placeholder="Role"
                   value={role}
                 />
                 <input
-                  className='profileInputs '
-                  type='email'
-                  placeholder='Email'
+                  className="profileInputs "
+                  type="email"
+                  placeholder="Email"
                   disabled
                   value={email}
                 />
                 <input
-                  className='profileInputs '
-                  type='text'
-                  placeholder='Name'
-                  onChange={handleChange('name')}
+                  className="profileInputs "
+                  type="text"
+                  placeholder="Name"
+                  onChange={handleChange("name")}
                   value={name}
                 />
 
                 <input
-                  className='profileInputs'
-                  type='password'
-                  placeholder='Password'
-                  onChange={handleChange('password1')}
+                  className="profileInputs"
+                  type="password"
+                  placeholder="Password"
+                  onChange={handleChange("password1")}
                   value={password1}
                 />
                 <button
-                  type='submit'
-                  className='profileSubmitBtn flex focus:shadow-outline'
+                  type="submit"
+                  className="profileSubmitBtn flex focus:shadow-outline"
                 >
-                  <i className='Icon fas fa-user-plus fa 1x ' />
-                  <span className='ml-3'>{textChange}</span>
+                  <i className="Icon fas fa-user-plus fa 1x " />
+                  <span className="ml-3">{textChange}</span>
                 </button>
               </div>
-              <div className='pageBreak'>
-                <div className='pageBreakText'>
-                  Go To Home
-                </div>
+              <div className="pageBreak">
+                <div className="pageBreakText">Go To Home</div>
               </div>
-              <div className='homeBtn flex'>
+              <div className="homeBtn flex">
                 <a
-                  className='homeBtnInterior flex focus:outline-none focus:shadow-outline '
-                  href='/'
-                  target='_self'
+                  className="homeBtnInterior flex focus:outline-none focus:shadow-outline "
+                  href="/"
+                  target="_self"
                 >
-                  <i className='fas fa-home fa 1x' />
-                  <span className='ml-4'>Home</span>
+                  <i className="fas fa-home fa 1x" />
+                  <span className="ml-4">Home</span>
                 </a>
               </div>
             </form>
           </div>
         </div>
-        <div className='map '>
-          <Map />
-          {/* <div
-            className='m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat'
-            style={{ backgroundImage: `url(${authSvg})` }}
-          ></div> */}
-        </div>
+        <div>
+                <input type="submit" value="Toggle Map On" onClick={toggleMapOn} />
+                <input type="submit" value="Toggle Map Off" onClick={toggleMapOff} />
+                {showMap ? <Map /> : null}
+            </div>
       </div>
       ;
     </div>
--- client/src/screens/profile/map/Map.jsx
+++ client/src/screens/profile/map/Map.jsx
@@ -1,61 +1,189 @@
 import React, { useState, useEffect } from "react";
 import {
-    withGoogleMap,
-    withScriptjs,
-    GoogleMap,
-    Marker,
-    InfoWindow
-} from "react-google-maps";
-import axios from "axios"
-import * as parkData from "./skateboard-parks.json";
-import mapStyles from "./mapStyles"
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import "./map.style.css";
+import mapStyles from "./mapStyles";
 import { UserRefreshClient } from "google-auth-library";
 import { toast } from "react-toastify";
+import { formatRelative } from "date-fns";
+import usePlacesAutoComplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = {
+  lat: 40,
+  lng: 60,
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+// -----------------working on toggle of map
+// class Mapp extends React.component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       isHidden:true
+//     }
+//   }
+//   toggleHidden () {
+//     this.setState({
+//       isHidden: !this.state.isHidden
+//     })
+//   }
+//   render() {
+//     return (
+//       <div> 
+//         <button onClick={this.toggleHidden.bind(this)} >
+//           click to toggle map
+//         </button>
+//         {!this.state.isHidden && <Map />}
+//       </div>
+//     )
+//   }
+// }
+
+// --------------------------------------------------------------------
 
 
-function Map() {
-if ('geolocation' in navigator ){
-console.log('geolocation available');
-navigator.geolocation.getCurrentPosition(position => {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
 
-    const data= {lat, lon};
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify(data)
-    };
-    fetch('/location', options);
-})
-} else {
-    console.log("error")
-}
-return (
-    <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
-      defaultOptions={{ styles: mapStyles }}
-    />
-)
+export default function Map() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+    libraries,
+  });
 
-}
-const MapWrapped = withScriptjs(withGoogleMap(Map));
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+ 
 
-export default function App() {
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  // use ref allows retain state without rerendering
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return "error loading maps";
+  if (!isLoaded) return "loading";
+
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <MapWrapped
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
-          process.env.REACT_APP_GOOGLE_KEY
-        }`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+    <div>
+     
+      <h1 className="maph1">Word on the Street</h1>
+
+      <Search />
+
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: `/vinylHead.jpg`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>User</h2>
+              <p>Was Here {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   );
 }
 
+function Search() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutoComplete({
+    requestOptions: {
+      location: { lat: () => 40, lng: () => 60 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={(address) => {
+        
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter an Address"
+        />
+        <ComboboxPopover>
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+
+
+// export default Map;
